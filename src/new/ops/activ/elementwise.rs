7b725946a42c00c802a9d1c43bc3c9e766d47678
zza <millard.jn@gmail.com>
2017-10-10 19:55:15 +1000

    Add Elementwise OpInstance; De-phantom the ActivationFunc trait

--- src/new/ops/activ/elementwise.rs
+++ src/new/ops/activ/elementwise.rs
@@ -1,34 +1,83 @@
-use new::graph::{NodeID, DataID, Storage, GraphShapes, ErrorKind, Result};
-use new::ops:: Pass;
+use new::graph::{GraphDef, NodeID, DataID, OpID, PassID, Storage, GraphShapes, ErrorKind, Result};
+use new::ops::{standard_op_name, Op, OpInstance, Pass};
 use std::any::Any;
-use std::marker::PhantomData;
 use std::fmt::Debug;
+
+
+
+pub fn elementwise_build<O: Op, F: ActivationFunc>(graph: &mut GraphDef, op: &O, name: &Option<String>, input: &NodeID, output: &NodeID, func: F) -> Result<ElementwiseInstance<F>> {
+	let name = standard_op_name(op, name, graph, &[input.clone()], &[output.clone()]);
+
+	Ok(ElementwiseInstance{
+		name: name,
+		input_id: input.clone(),
+		output_id: output.clone(),
+		func: func.clone(),
+		forward_id: graph.add_pass(ElementwiseForward::new(
+				input.clone(),
+				output.clone(),
+				func.clone())),
+		backward_id: graph.add_pass(ElementwiseBackward::new(
+				input.clone(),
+				output.clone(),
+				func.clone())),
+	})
+}
+
 
 /// Used to define graph op with no parameters, where the effect of the input on the output is entirely seperable.
 pub trait ActivationFunc: Clone + Debug + 'static {
 	#[inline(always)]
 	/// For a given input x, what is the output y, and the derivative dy/dx
-	fn value(input: f32) -> f32;
+	fn value(&self, input: f32) -> f32;
 
-	fn gradient(input: f32, output_grad: f32) -> f32;
+	fn gradient(&self, input: f32, output_grad: f32) -> f32;
 
 	fn backprop_requires_input_value() -> bool;
 }
 
-
 #[derive(Clone, Debug)]
-struct ElementwiseForward<F: ActivationFunc> {
+pub struct ElementwiseInstance<F: ActivationFunc> {
+	name: String,
 	input_id: NodeID,
 	output_id: NodeID,
-	func: PhantomData<F>,
+	func: F,
+	forward_id: PassID,
+	backward_id: PassID,
+}
+
+impl<F: ActivationFunc> OpInstance for ElementwiseInstance<F> {
+
+	fn instance_name(&self) -> &str{&self.name}
+
+	fn dependencies(&self) -> (Vec<NodeID>, Vec<NodeID>){(vec![self.input_id.clone()], vec![self.output_id.clone()])}
+
+	fn inner_passes(&self) -> Vec<PassID>{vec![self.forward_id.clone(), self.backward_id.clone()]}
+
+	fn inner_ops(&self) -> Vec<OpID>{vec![]}
+
+	fn inner_nodes(&self) -> Vec<NodeID>{vec![]}
+
+	fn propagate_shape_constraints(&self, shapes: &mut GraphShapes) -> Result<()>{
+		let input_shape = shapes.get_shape(&self.input_id).clone();
+		shapes.merge_with(&self.output_id, &input_shape)
+	}
+}
+
+
+#[derive(Clone, Debug)]
+pub struct ElementwiseForward<F: ActivationFunc> {
+	input_id: NodeID,
+	output_id: NodeID,
+	func: F,
 }
 
 impl<F: ActivationFunc> ElementwiseForward<F> {
-	pub fn new(input_id: NodeID, output_id: NodeID) -> Self {
+	pub fn new(input_id: NodeID, output_id: NodeID, func: F) -> Self {
 		ElementwiseForward {
 			input_id,
 			output_id,
-			func: PhantomData,
+			func,
 		}
 	}
 }
@@ -61,7 +110,7 @@ impl<F: ActivationFunc> Pass for ElementwiseForward<F> {
 		let out = &mut output[..len];
 
 		for i in 0..len{
-			out[i] += F::value(inp[i]);
+			out[i] += self.func.value(inp[i]);
 		}
 
 		Ok(Box::new(()))
@@ -70,18 +119,18 @@ impl<F: ActivationFunc> Pass for ElementwiseForward<F> {
 
 
 #[derive(Clone, Debug)]
-struct ElementwiseBackward<F: ActivationFunc> {
+pub struct ElementwiseBackward<F: ActivationFunc> {
 	input_id: NodeID,
 	output_id: NodeID,
-	func: PhantomData<F>,
+	func: F,
 }
 
 impl<F: ActivationFunc> ElementwiseBackward<F> {
-	pub fn new(input_id: NodeID, output_id: NodeID) -> Self {
+	pub fn new(input_id: NodeID, output_id: NodeID, func: F) -> Self {
 		ElementwiseBackward {
 			input_id,
 			output_id,
-			func: PhantomData,
+			func,
 		}
 	}
 }
@@ -127,7 +176,7 @@ impl<F: ActivationFunc> Pass for ElementwiseBackward<F> {
 			let inpd = &mut input_grad[..len];
 
 			for i in 0..len{
-				inpd[i] += F::gradient(inp[i], outd[i]);
+				inpd[i] += self.func.gradient(inp[i], outd[i]);
 			}
 		} else {
 
@@ -135,7 +184,7 @@ impl<F: ActivationFunc> Pass for ElementwiseBackward<F> {
 			let inpd = &mut input_grad[..len];
 
 			for i in 0..len{
-				inpd[i] += F::gradient(0.0, outd[i]);
+				inpd[i] += self.func.gradient(0.0, outd[i]);
 			}
 		}
 
