40ee5599f52908a2252354c4f80e138f3c673894
zza <millard.jn@gmail.com>
2017-10-26 21:52:44 +1000

    Add basic optimiser callback generators

--- src/new/opt/mod.rs
+++ src/new/opt/mod.rs
@@ -51,13 +51,53 @@ pub trait Opt {
 
 			let data = CallbackData{err: err, params: &params, stream: training_stream};
 			for func in self.callbacks().iter_mut(){
-				
-				match func(&data){
-					CallbackSignal::Stop => {stop = true},
-					CallbackSignal::Continue =>{},
-				}
+				stop = matches!(func(&data), CallbackSignal::Stop);
 			}
 		}
 		Ok(params)
 	}
+}
+
+
+pub fn print_step_data() -> Box<FnMut(&CallbackData)->CallbackSignal>{
+	let mut step = 0;
+	Box::new(move |data|{
+		println!("step:{}\terr:{}", step, data.err);
+		step += 1;
+		CallbackSignal::Continue
+	})
+}
+
+pub fn max_steps(max: usize) -> Box<FnMut(&CallbackData)->CallbackSignal>{
+	let mut step = 0;
+	Box::new(move |_data|{
+		if step < max {
+			step += 1;
+			CallbackSignal::Continue
+		} else {
+			CallbackSignal::Stop
+		}
+	})
+}
+
+pub fn min_err(min: f32) -> Box<FnMut(&CallbackData)->CallbackSignal>{
+	Box::new(move |data|{
+		if data.err > min {
+			CallbackSignal::Continue
+		} else {
+			CallbackSignal::Stop
+		}
+	})
+}
+
+pub fn every_n_steps(n: usize, mut func: Box<FnMut(&CallbackData)->CallbackSignal>) -> Box<FnMut(&CallbackData)->CallbackSignal>{
+	let mut step = 0;
+	Box::new(move |data|{
+		if step % n == 0 {
+			func(data)
+		} else {
+			step += 1;
+			CallbackSignal::Continue
+		}
+	})
 }
