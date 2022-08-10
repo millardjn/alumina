636409f92e62c0644e1c23cc7c5c22205212a3e7
James Millard <millard.jn@gmail.com>
2022-08-10 21:11:57 +1000

    EWMA bug fixes

--- alumina_opt/src/ewma.rs
+++ alumina_opt/src/ewma.rs
@@ -31,7 +31,7 @@ impl Ewma {
 
 	// Return the latest EMWA for each node
 	pub fn get(&self) -> IndexMap<Node, ArrayD<f32>> {
-		let correction = 1.0 / (self.beta.powi(self.update_count)).max(1.0);
+		let correction = 1.0 / (1.0 - self.beta.powi(self.update_count));
 		self.values
 			.iter()
 			.map(|(node, v)| (node.clone(), v * correction))
@@ -42,20 +42,19 @@ impl Ewma {
 	pub fn update(&mut self) {
 		let beta = self.beta;
 		for (node, value) in &mut self.values {
-			value.map_inplace(|e| *e *= beta);
 			Zip::from(value)
 				.and(&node.value().expect("Emwa node did not have a value set"))
 				.for_each(|emwa, next| {
-					*emwa = (1.0 - beta) * next;
+					*emwa = *emwa*beta + (1.0 - beta) * next;
 				});
 		}
+		self.update_count += 1;
 	}
 
 	// Update the EMWA from custom node values
 	pub fn update_custom(&mut self, values: IndexMap<Node, ArrayD<f32>>) {
 		let beta = self.beta;
 		for (node, value) in &mut self.values {
-			value.map_inplace(|e| *e *= beta);
 			Zip::from(value)
 				.and(
 					values
@@ -63,7 +62,7 @@ impl Ewma {
 						.expect("Custom emwa values did not contain all required nodes"),
 				)
 				.for_each(|emwa, next| {
-					*emwa = (1.0 - beta) * next;
+					*emwa = *emwa*beta + (1.0 - beta) * next;
 				});
 		}
 	}
